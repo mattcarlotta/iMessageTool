2017f5e5aff92abda9e0b7e856a72ed2986d8e68
Matt Carlotta <carlotta.matt@gmail.com>
2017-03-12 13:38:32 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,121 @@
+# iMessageTool
+
+Introduction
+
+iMessageTool is a simple automated bash script that either deletes iMessage files and folders stored within a user's "Library" folder OR generates 20 unique ids(SmUUIDs).
+
+You can download the latest version of iMessageTool to your Desktop by entering the following command in a terminal window:
+```
+curl -o ~/Desktop/iMessageTool.sh https://raw.githubusercontent.com/mattcarlotta/iMessageTool/master/iMessageTool.sh
+```
+You can then verify the downloaded size (should be about 6kb):
+```
+wc -c ~/Desktop/iMessageTool.sh
+```
+Then, you must change the file permissions to make it executable:
+```
+chmod +x ~/Desktop/iMessageTool.sh
+```
+Lastly, use this command to run the script:
+```
+~/Desktop/iMessageTool.sh
+```
+
+Commands:
+```
+dic (will delete iMessage caches)
+idgen (will generate SmUUIDs)
+help (will display instructions)
+exit (will exit the script)
+```
+--------------------------------------------------------------------------------------------------------------
+
+You'll be initially greeted with:
+```
+This script must be run as ROOT!
+```
+Input your Mac OS password to continue.
+
+--------------------------------------------------------------------------------------------------------------
+
+You'll then greeted with:
+```
+Would you like to delete iMessage caches or generate SmUUIDs? (dic/idgen/help/exit)?
+```
+
+If you input "idc" or "IDC" without quotes, then press the Enter key:
+```
+idc
+```
+
+The following folders/files will be deleted:
+```
+Folders in {user}/Library/Caches:
+1.) com.apple.iCloudHelper
+2.) com.apple.imfoundation.IMRemoteURLConnectionAgent
+3.) com.apple.Messages
+
+Files in {user}/Library/Preferences:
+1.) com.apple.iChat*
+2.) com.apple.icloud*
+3.) com.apple.ids.service*
+4.) com.apple.imagent*
+5.) com.apple.imessage*
+6.) com.apple.imservice*
+
+Folder and files in {user}/Library/Messages
+1.) Archive
+2.) chat.db
+3.) chat.db-shm
+4.) chat.db-wal
+```
+You'll then be greeted with:
+```
+All related iMessage files and folders have been successfully removed!
+
+Do you want to reboot now? (y/n)
+```
+
+Type "y" or "Y" without quotes to reboot your computer and press the Enter key. Otherwise, you can type "n" or "N" to abort the script and restart your OS manually.
+
+--------------------------------------------------------------------------------------------------------------
+
+If you input "idgen" or "IDGEN" without quotes, then press the Enter key:
+```
+idgen
+```
+You will then see an output like:
+```
+1: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+...
+20: XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX
+```
+Now you copy and use any of the generated SmUUIDs!
+
+--------------------------------------------------------------------------------------------------------------
+
+If you input "macgen" or "MACGEN" without quotes, then press the Enter key:
+```
+macgen
+```
+You will then see an output like:
+```
+NOTE: No input product name found, using IORegistry value ({SMBIOS})
+NOTE: No input serial number found, using IORegistry value ({Board Serial})
+Manufacturing location (from serial number): {MANUFACTURER}
+Manufacturing year (from serial number): {YEAR}
+Manufacturing week (from serial number): {WEEK}
+Generated board type: XXX
+Generated CC value: XX
+Generated EEEE value: XXXX
+Generated KK value: XX
+
+Generated {SMBIOS} MLB serial number: XXXXXXXXXXXXXXXXX
+```
+Now you copy and use the generated MLB serial number!
+
+Note: The author of MacGen is theracermaster and you can find his repo here: <a href="https://github.com/theracermaster/MacGen">MacGen</a>
+
+--------------------------------------------------------------------------------------------------------------
+
+**Note: If at any point you type an invalid character, then that will temporarily stop the script. Wait a few seconds and it will restart on its own!
---
+++ iMessageTool.sh
@@ -0,0 +1,280 @@
+#!/bin/bash
+#
+# iMessageTool Version 2.1.0 - Copyright (c) 2016 by M.F.C.
+#
+# Introduction:
+#     - iMessageTool is a simple automated bash script that removes cached
+#        iMessage files and folders or generates user specified SmUUIDs!
+#     - Simply unzip the iMessageTool file to your Desktop and
+#        double click to execute the script!
+#
+#
+# Bugs:
+#     - Bug reports can be filed at: https://github.com/mfc88/iMessageTool/issues
+#        Please provide clear steps to reproduce the bug and the output of the
+#        script. Thank you!
+#
+
+#===============================================================================##
+## GLOBAL VARIABLES #
+##==============================================================================##
+errNum=0
+
+# Get current user
+gUSER=$(stat -f%Su /dev/console)
+
+# User's current directory
+gRepo="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+# Kext path, will be overrided by _find_kext()
+gpathMG=""
+
+# Kext name
+gMG="mg-mlb-serial"
+
+# AII.kext Repo
+gRacerMG="https://github.com/theracermaster/MacGen"
+
+#===============================================================================##
+## USER ABORTS SCRIPT #
+##==============================================================================##
+function clean_up()
+{
+  clear
+  exit 1
+}
+
+#===============================================================================##
+## ERROR HANDLING #
+##==============================================================================##
+function error ()
+{
+  errNum=$1
+
+  if [[ $errNum -eq 0 ]];
+    then
+      # user mistake at main
+      printf '\n'
+      printf '*—-ERROR—-* Invalid choice! Please input “dic” or “idgen” at the prompt.\n'
+      printf '            Otherwise, type "help" at the prompt for instructions!\n'
+      printf '\n'
+      printf 'Restarting the script...\n'
+      sleep 2.5
+      main
+    else
+      # user inputs invalid choice at help
+      printf '\n'
+      printf '*—-ERROR—-* Invalid input.\n'
+      printf '\n'
+      printf 'Restarting the script...\n'
+      sleep 2.5
+      main
+  fi
+}
+
+#===============================================================================##
+## DISPLAY INSTRUCTIONS #
+##==============================================================================##
+function display_instructions()
+{
+  printf "\n"
+  printf "To delete iMessage cache files and folders, input 'dic' or 'DIC'\n"
+  printf "       -At next prompt, type 'y' or 'Y' to begin deleting the caches\n"
+  printf "       -At next prompt, type 'y' or 'Y' to reboot your computer\n"
+  printf "\n"
+  printf "To generate SmUUIDs, input 'idgen' or 'IDGEN'\n"
+  printf "       -Will automatically generate 20 unique IDs\n"
+  printf "\n"
+  printf "To generate a MLB Serial, input 'macgen' or 'MACGEN'\n"
+  printf "       -Will automatically generate a unique MLB Serial\n"
+  printf "\n"
+  read -p "Would you like to reload the script now? (y/n)? " choice
+    case "$choice" in
+    y|Y )
+    main
+    ;;
+    n|N )
+    clean_up
+    ;;
+    * )
+    error 1
+    ;;
+esac
+}
+
+#===============================================================================##
+## REBOOT COMPUTER #
+##==============================================================================##
+function reboot_computer()
+{
+  printf '\n'
+  read -p "Would you like to reboot now? (y/n) " choice
+    case "$choice" in
+    # user requests reboot
+    y|Y )
+    reboot now
+    ;;
+    # user refuses reboot, terminate script
+    n|N )
+    printf "\n"
+    printf "It’s highly recommended that you restart your computer as soon as possible!\n"
+    printf "\n"
+    exit 1
+    ;;
+    # oops - user made a mistake, terminate script anyway
+    * )
+    printf "\n"
+    printf '*—-ERROR—-* Invalid choice! If deleting the iMessage caches was successful,\n'
+    printf 'then it’s highly recommended that you restart your computer as soon as possible!\n'
+    printf "\n"
+    printf 'Script was aborted!\n'
+    exit 1
+    ;;
+    esac
+}
+
+#===============================================================================##
+## DELETE IMESSAGE CACHES #
+##==============================================================================##
+function del_caches()
+{
+  # remove cache folders in Library/Caches
+  cd ~/Library/Caches
+  rm -rf com.apple.iCloudHelper com.apple.imfoundation.IMRemoteURLConnectionAgent com.apple.Messages
+  # remove files from Library/Preferences
+  cd ~/Library/Preferences
+  rm com.apple.iChat* com.apple.icloud* com.apple.ids.service* com.apple.imagent* com.apple.imessage* com.apple.imservice*
+  # remove messages from library/messages
+  find ~/Library/Messages/ -mindepth 1 -delete
+  printf '\n'
+  printf 'All related iMessage files and folders have been successfully removed!\n'
+  sleep 0.25
+  reboot_computer
+}
+
+#===============================================================================##
+## Load MacGen #
+##==============================================================================##
+function load_macgen()
+{
+  printf " \n"
+  printf "${gMG} was successfully loaded...\n"
+  chown $gUSER $gdirectoryMG
+  gpathMG="${gdirectoryMG}/${gMG}"
+  printf " \n"
+  ${gpathMG}
+}
+
+#===============================================================================##
+## Find MacGen #
+##==============================================================================##
+function find_macgen()
+{
+  # Directory path
+  local gdirectoryMG="$HOME/Desktop/MacGen"
+
+  printf " \n"
+  printf "Searching for the MacGen directory on the Desktop...\n"
+  if [[ ! -d "${gdirectoryMG}" ]];
+    then
+      printf " \n"
+      printf "${gMG} wasn't found on the Desktop!\n"
+      printf " \n"
+      printf "Downloading MacGen from theracermaster's GitHub instead...\n"
+      printf " \n"
+      cd ~/Desktop
+      git clone "${gRacerMG}"
+      if [[ $? -ne 0 ]];
+        then
+          printf ' \n'
+          printf '*—-ERROR—-* Make sure your network connection is active!\n'
+          exit 1
+        else
+          load_macgen
+      fi
+    else
+      load_macgen
+  fi
+}
+
+#===============================================================================##
+## GENERATE SMUUIDS #
+##==============================================================================##
+function generate_ids()
+{
+      for (( i=1; i <= 20; i++ ))
+      do
+        printf "%s" "$i: "
+        uuidgen
+      done
+}
+
+#===============================================================================##
+## MAIN #
+##==============================================================================##
+function user_choices()
+{
+  read -p "Delete iMessage caches or generate SmUUIDs? (dic/idgen/macgen/help/exit)? " choice
+    case "$choice" in
+      # call delete caches
+      dic|DIC )
+      del_caches
+      ;;
+      # call generate ids
+      idgen|IDGEN )
+      generate_ids
+      printf "\n"
+      printf "All done!"
+      printf "\n"
+      exit 0
+      ;;
+      # call macgen for MLB serial
+      macgen|MACGEN )
+      find_macgen
+      ;;
+      # display help instructions
+      help|HELP )
+      display_instructions
+      ;;
+      # kill the script
+      exit|EXIT )
+      clean_up
+      ;;
+      # oops - user made a mistake, reload script
+      * )
+      error
+      ;;
+    esac
+}
+
+#===============================================================================##
+## GREET USER #
+##==============================================================================##
+function greet()
+{
+  printf '            iMessageTool Version 2.1.0 - Copyright (c) 2016 by M.F.C.'
+  printf  "\n%s" '--------------------------------------------------------------------------------'
+  printf ' \n'
+  sleep 0.25
+}
+
+#===============================================================================##
+## START PROGRAM #
+##==============================================================================##
+function main() {
+  clear
+  greet
+  user_choices
+}
+
+trap '{ clean_up; exit 1; }' INT
+
+if [[ `id -u` -ne 0 ]];
+  then
+    printf "This script must be run as ROOT!\n"
+    sudo "$0"
+  else
+    main
+fi
+
+#================================================================================
